7d6a74c21f75fd15454c8541939f8a2864c40cb7
Pulak <pulakkumarghosh2001@gmail.com>
2023-09-26 08:31:22 +0530

    Apk update

--- src/component/HomePage.jsx
+++ src/component/HomePage.jsx
@@ -95,7 +95,7 @@ export default function HomePage() {
             <h1 className="text-light">INDIAGREEN</h1>
             <p className="text-light">Welcome to new edge technology</p>
             <div className="mt-5 d-flex flex-column flex-lg-row" >
-              <div onClick={() => window.location.href='/India-Green-v59.apk'} style={{cursor: 'pointer'}} className="d-flex flex-row align-items-center justify-content-center border-1 border-light border p-2 rounded-3 m-2">
+              <div onClick={() => window.location.href='/India-Green-v60.apk'} style={{cursor: 'pointer'}} className="d-flex flex-row align-items-center justify-content-center border-1 border-light border p-2 rounded-3 m-2">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-google-play" viewBox="0 0 16 16">
                   <path d="M14.222 9.374c1.037-.61 1.037-2.137 0-2.748L11.528 5.04 8.32 8l3.207 2.96 2.694-1.586Zm-3.595 2.116L7.583 8.68 1.03 14.73c.201 1.029 1.36 1.61 2.303 1.055l7.294-4.295ZM1 13.396V2.603L6.846 8 1 13.396ZM1.03 1.27l6.553 6.05 3.044-2.81L3.333.215C2.39-.341 1.231.24 1.03 1.27Z"/>
                 </svg>
